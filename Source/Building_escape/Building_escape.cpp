ac269857ece82f68deb92a9ca01d8980ea0372ae
gelserkirchen@Mail.ru <Tatevik1993$>
2020-03-05 08:32:19 +0300

    Initial commit

---
+++ Source/Building_escape/Building_escape.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "Building_escape.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Building_escape, "Building_escape" );
---
+++ Source/Building_escape/Building_escapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Building_escapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDING_ESCAPE_API ABuilding_escapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
